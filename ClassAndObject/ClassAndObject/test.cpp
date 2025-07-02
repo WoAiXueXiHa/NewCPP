99bbcb58629dba6a79b656f2d01176d5f8d5418e
Coke <1760198676@qq.com>
2025-07-02 21:41:29 +0800

    1

---
+++ ClassAndObject/ClassAndObject/test.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//class A {
+//public:
+//    void Print() {
+//        cout << "Print()" << endl;
+//    }
+//private:
+//    int x;  
+//};
+//
+//int main() {
+//    A* p = nullptr; 
+//    p->Print();       
+//    return 0;
+//}
