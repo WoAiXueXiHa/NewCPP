f32f1b84bb6f855fbaa914dd583e872ba3237efd
Coke <1760198676@qq.com>
2025-07-06 17:10:54 +0800

    11

--- ClassAndObject/ClassAndObject/test.cpp
+++ ClassAndObject/ClassAndObject/test.cpp
@@ -305,69 +305,127 @@ using namespace std;
 //	return 0;
 //}
 
-// 构造函数
-// 1. 函数名和类名同名 2. 可以重载 3. 没有返回值 4. 用户不写编译器会默认生成无参的构造函数
+//// 构造函数
+//// 1. 函数名和类名同名 2. 可以重载 3. 没有返回值 4. 用户不写编译器会默认生成无参的构造函数
+//class Date
+//{
+//public:
+//	// 无参构造
+//	Date()
+//	{
+//		_year = 1;
+//		_month = 1;
+//		_day = 1;
+//	}
+//
+//	// 带参数构造
+//	Date(size_t year, size_t month, size_t day)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//
+//	//// 全缺省构造
+//	//Date(size_t year = 1, size_t month = 1, size_t day = 1)
+//	//{
+//	//	_year = year;
+//	//	_month = month;
+//	//	_day = day;
+//	//}
+//	//
+//	void Print()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//private:
+//	size_t _year;
+//	size_t _month;
+//	size_t _day;
+//};
+//
+//int main()
+//{
+//	// 调用带参数的构造
+//	Date d1(2025,7,5);
+//	d1.Print();
+//
+//	//// 无参构造和全缺省构造会产生调用歧义
+//	//Date d2;
+//	//d2.Print();
+//
+//	// 无参的不能这么写 会和函数声明搞混 eg: void func
+//	// 这是函数声明还是函数定义呢？
+//	/*Date d2();
+//	d2.Print();*/
+//
+//	//// 如果注释掉无参的构造和全缺省构造，会报错
+//	//// C2512 没有合适的默认构造函数可用
+//	//Date d2;
+//	//d2.Print();
+//
+//	// 调用无参的构造函数
+//	Date d3;
+//	d3.Print();
+//
+//	return 0;
+//}
+//using namespace std;
+//class Time
+//{
+//public:
+//	/*Time()
+//	{
+//		_hour = 1;
+//		_minute = 1;
+//		_second = 1;
+//	}*/
+//
+//private:
+//	size_t _hour;
+//	size_t _minute;
+//	size_t _second;
+//};
+//
+//class Date
+//{
+//public:
+//	// 不写构造函数 编译器会自动生成默认构造函数
+//	// 对于内置类型 编译器是否处理没有明确要求
+//	// 对于自定义类型 调用该类型的默认构造函数
+//
+//	void Print()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//private:
+//	size_t _year;	
+//	size_t _month;
+//	size_t _day;
+//	Time _t;
+//};
+//
+//int main()
+//{
+//	Date d1;
+//	d1.Print();
+//	
+//	return 0;
+//}
 
-class Date
+// 析构函数
+// 1. 函数名和类名同名，函数名前加'~' 2. 不能重载 3. 用户不显式定义，编译器会默认生成析构函数
+
+class Stack
 {
 public:
-	// 无参构造
-	Date()
+	Stack()
 	{
-		_year = 1;
-		_month = 1;
-		_day = 1;
-	}
-
-	// 带参数构造
-	Date(size_t year, size_t month, size_t day)
-	{
-		_year = year;
-		_month = month;
-		_day = day;
-	}
-
-	//// 全缺省构造
-	//Date(size_t year = 1, size_t month = 1, size_t day = 1)
-	//{
-	//	_year = year;
-	//	_month = month;
-	//	_day = day;
-	//}
-	//
-	void Print()
-	{
-		cout << _year << "-" << _month << "-" << _day << endl;
+		if
 	}
 private:
-	size_t _year;
-	size_t _month;
-	size_t _day;
+	int* _arr;
+	int _capacity;
+	int _top;
+
 };
-
-int main()
-{
-	// 调用带参数的构造
-	Date d1(2025,7,5);
-	d1.Print();
-	//// 无参构造和全缺省构造会产生调用歧义
-	//Date d2;
-	//d2.Print();
-
-	// 无参的不能这么写 会和函数声明搞混 eg: void func
-	// 这是函数声明还是函数定义呢？
-	/*Date d2();
-	d2.Print();*/
-
-	//// 如果注释掉无参的构造和全缺省构造，会报错
-	//// C2512 没有合适的默认构造函数可用
-	//Date d2;
-	//d2.Print();
-
-	// 调用无参的构造函数
-	Date d3;
-	d3.Print();
-
-	return 0;
-
-}
