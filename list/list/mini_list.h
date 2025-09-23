105139357a663cd7e71d5791d87745349dcc7849
Coke <1760198676@qq.com>
2025-09-23 19:29:33 +0800

    更新

---
+++ list/list/mini_list.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+namespace Vect {
+	
+	// 节点类 节点会被链表频繁访问 所以数据公开
+	template <class T>
+	struct node {
+		
+	};
+
+	// 链表类
+	template <class T>
+	class list {
+	private:
+
+	public:
+	
+	};
+}
+
