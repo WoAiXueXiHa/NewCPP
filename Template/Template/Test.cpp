3f1e48cad1a971b6b201de7d507aa9dc28509c46
Coke <1760198676@qq.com>
2025-10-14 12:47:52 +0800

    模板常用用法

--- Template/Template/Test.cpp
+++ Template/Template/Test.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <stdbool.h>
 using namespace std;
 
+/*=============================== 初阶 ==========================================*/
 //// 思考：如何实现一个通用的交换函数？
 //// 传统方式：函数重载，但是遇到类型不同，反复的写重复的代码
 //void Swap(int& left, int& right)
@@ -181,3 +183,158 @@ using namespace std;
 //// Vector类名 Vector<int>才是类型
 //Vector<int> t1;
 //Vector<double> t2;
+
+
+/*=============================== 进阶 ==========================================*/
+
+//// 1. 非类型模板参数
+//template<typename T, size_t size = 10>
+//class Array {
+//public:
+//	T& operator[](size_t index) { return _array[index]; }
+//	const T& operator[](size_t index) const { return _array[index]; }
+//	size_t size() const { return _size; }
+//	bool empty() const { return _size == 0; }
+//private:
+//	T _array[size];
+//	size_t _size;
+//};
+
+// 2. 模板的特化
+//// 2.1.实现一个小于比较的函数模板
+//template<class T>
+//bool Less(const T& left,  const T& right) { return left < right; }
+//
+//class Date {
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//public:
+//	Date(int year = 2025, int month = 10, int day = 15)
+//		:_year(year)
+//		,_month(month)
+//		,_day(day)
+//	{ }
+//	bool operator<(const Date& other) const noexcept {
+//		if (_year < other._year) return true;
+//		if (_year > other._year) return false;
+//
+//		if (_month < other._month) return true;
+//		if (_month > other._month) return false;
+//
+//		return _day < other._day; 
+//	}
+//};
+//// 特化指针比较
+//template<>
+//// const T& val 修饰的是这个值不可修改 
+//// 那么对应指针就是指向的内容不可修改 而不是指针不能修改
+//// 这个写法太怪了 函数模板特化不建议使用 需要特化的版本直接实现一个函数更好
+//bool Less<Date*>(Date* const& left, Date* const& right) { return *left < *right; }
+//
+//bool Less(Date* left, Date* right) { return *left < *right; }
+//int main() {
+//	Date d1(2025, 10, 20);
+//	Date d2(2025, 10, 15);
+//	Date* ptr1 = &d1;
+//	Date* ptr2 = &d2;
+//	cout << Less(ptr1, ptr2) << endl; 
+//}
+
+//int main() {
+//	cout << Less(1, 2) << endl; // 正确
+//	cout << Less('X', 'Y') << endl; // 正确
+//	
+//	Date d1(2025, 10, 20);
+//	Date d2(2025, 10, 15);
+//	cout << Less(d1, d2) << endl; // 比较正确
+//
+//	// 这里比较的是地址 并非指向的内容
+//	Date* ptr1 = &d1;
+//	Date* ptr2 = &d2;
+//	cout << Less(ptr1, ptr2) << endl; // 可以比较 结果错误 
+//}
+
+// 2.2. 全特化
+// 类模板
+template <class T1, class T2>
+class Show {
+public:
+	Show() { cout << "Show<T1,T2>"<<endl; }
+private:
+	T1 _showInt;
+	T2 _showChar;
+};
+// 全特化
+//template<>
+//class Show<int, char> {
+//public:
+//	Show() { cout << "Show<int,char>"<<endl; }
+//private:
+//	int _showInt;
+//	char _showChar;
+//};
+//
+//int main() {
+//	Show<int, int> s1;
+//	Show<int, char> s2;
+//}
+
+// 2.3. 偏特化
+// 1>将第一个参数特化为int
+template <class T2>
+class Show<int,T2> {
+public:
+	Show() { cout << "Show<int,T2>" << endl; }
+private:
+	int _showInt;
+	T2 _showChar;
+};
+
+// 2>  按”两个参数同种类型“特化 两个参数特化为指针类型
+template <typename T1, typename T2>
+class Show<T1*,T2*> {
+public:
+	Show() { cout << "Show<T1*,T2*>" << endl; }
+private:
+	T1* _showInt;
+	T2* _showChar;
+};
+
+//int main() {
+//	Show<int, double> s1;
+//	Show<char, double> s2;
+//	Show<int*, double*> s3;
+//
+//	return 0;
+//}
+#include <vector>
+template <class T>
+void printVector(const vector<T>& data) {
+	typename vector<T>::const_iterator it = data.begin();
+	while (it != data.end()) {
+		cout << *it << " ";
+		++it;
+	}
+	cout << endl;
+}
+
+int main() {
+	vector<int> arr1 = { 1, 2, 3, 4, 5 };
+	vector<double> arr2 = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+	printVector(arr1);
+	printVector(arr2);
+}
+
+template <class T>
+struct Has {
+	template <class U>
+	void bar(U) {}
+};
+
+template <class X>
+void call(Has<X>& h) {
+	//  这里要写 template，表示 bar 是“成员模板”
+	h.template bar<int>(42);
+}
